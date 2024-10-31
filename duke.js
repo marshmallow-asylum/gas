37da53a2e9ec632c14a6a4c11fa6bd114f0ce7eb
smorez <smorez.ka@outlook.com>
2024-10-31 17:26:43 -0400

    slack.ka @ 1730410003275

--- duke.js
+++ duke.js
@@ -1,32 +1,32 @@
 var chatJson = {
     "chat": [
         {
-            "time": "13 minutes ago",
+            "time": "14 minutes ago",
             "text": "Awaiting Duke to change the program.\n...",
             "user": "duke"
         },
         {
-            "time": "13 minutes ago",
+            "time": "14 minutes ago",
             "text": "wow that was fast\nhey duke :) i'm the director and lead dev for codevera. pleasure to meet you.",
             "user": "ski"
         },
         {
-            "time": "13 minutes ago",
+            "time": "14 minutes ago",
             "text": "I love slack",
             "user": "astro"
         },
         {
-            "time": "12 minutes ago",
+            "time": "13 minutes ago",
             "text": "that was gas not slack lol",
             "user": "ski"
         },
         {
-            "time": "12 minutes ago",
+            "time": "13 minutes ago",
             "text": "I also love gas as I've previously mentioned",
             "user": "astro"
         },
         {
-            "time": "12 minutes ago",
+            "time": "13 minutes ago",
             "text": "you love too much",
             "user": "ski"
         }
