231a7ea3d08420f70d7eaae83fb5b52521a0d979
smorez <smorez.ka@outlook.com>
2024-10-31 17:14:44 -0400

    slack.ka @ 1730409283412

--- duke.js
+++ duke.js
@@ -1,29 +1,34 @@
 var chatJson = {
     "chat": [
         {
-            "time": "a minute ago",
+            "time": "2 minutes ago",
             "text": "Awaiting Duke to change the program.\n...",
             "user": "duke"
         },
         {
-            "time": "a minute ago",
-            "text": "wow that was fast",
+            "time": "2 minutes ago",
+            "text": "wow that was fast\nhey duke :) i'm the director and lead dev for codevera. pleasure to meet you.",
             "user": "ski"
         },
         {
-            "time": "a few seconds ago",
+            "time": "2 minutes ago",
             "text": "I love slack",
             "user": "astro"
         },
         {
-            "time": "a few seconds ago",
+            "time": "a minute ago",
             "text": "that was gas not slack lol",
             "user": "ski"
         },
         {
-            "time": "a few seconds ago",
+            "time": "a minute ago",
             "text": "I also love gas as I've previously mentioned",
             "user": "astro"
+        },
+        {
+            "time": "a few seconds ago",
+            "text": "you love too much",
+            "user": "ski"
         }
     ],
     "hash": "4225074d4e29bf049913ef44d76f3bce"
