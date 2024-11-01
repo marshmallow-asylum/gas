442a3212c5881a2d21587c3e1fa0611d991ea289
smorez <smorez.ka@outlook.com>
2024-11-01 11:21:43 -0400

    slack.ka @ 1730474503012

--- duke.js
+++ duke.js
@@ -31,7 +31,7 @@ var chatJson = {
             "user": "ski"
         },
         {
-            "time": "a few seconds ago",
+            "time": "2 minutes ago",
             "text": "Can you guys see this? Only the loading screen is showing up for me...",
             "user": "duke"
         }
