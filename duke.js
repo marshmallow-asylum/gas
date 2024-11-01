816142576618c9b8bcf8e9d2636acaad3f5b3002
smorez <smorez.ka@outlook.com>
2024-11-01 14:46:44 -0400

    slack.ka @ 1730486804390

--- duke.js
+++ duke.js
@@ -36,12 +36,12 @@ var chatJson = {
             "user": "duke"
         },
         {
-            "time": "18 minutes ago",
+            "time": "19 minutes ago",
             "text": "this is quite the annoyance",
             "user": "ski"
         },
         {
-            "time": "16 minutes ago",
+            "time": "17 minutes ago",
             "text": "also if you see this, yes, i'm the one managin' the s'morez acc an' \"the developer\" mentioned. if i admit i'm ski on that account, it will get insta-banned. my name is on a blacklist",
             "user": "ski"
         }
