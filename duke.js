881d390e6dc2da8b0e398e6553cf0e62dbe8fcdc
smorez <smorez.ka@outlook.com>
2024-11-02 16:42:43 -0400

    slack.ka @ 1730580162788

--- duke.js
+++ duke.js
@@ -56,7 +56,7 @@ var chatJson = {
             "user": "ski"
         },
         {
-            "time": "17 hours ago",
+            "time": "18 hours ago",
             "text": "Hey guys! Not sure if this will work. But I just found out a possible form of commuication... You should just need a google drive account. Tell me if you think would work.(and if it works on your end) <https://docs.google.com/spreadsheets/d/1VPm5-XyFNJC9Y3oREkfLzxODDaV5l2ns7pVrWNgPCXQ/edit?usp=sharing>",
             "user": "duke"
         },
