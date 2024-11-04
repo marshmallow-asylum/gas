5e99fe282b7e03a9bc8ef6d7d4656f8f807dc663
smorez <smorez.ka@outlook.com>
2024-11-04 10:37:45 -0500

    slack.ka @ 1730734664922

--- duke.js
+++ duke.js
@@ -76,7 +76,7 @@ var chatJson = {
             "user": "duke"
         },
         {
-            "time": "an hour ago",
+            "time": "2 hours ago",
             "text": "Hey ski, could it be my ka settings?",
             "user": "duke"
         }
