3cf8d8b2e4d77b26693024615602c2c937a46c79
smorez <smorez.ka@outlook.com>
2024-11-04 23:16:45 -0500

    slack.ka @ 1730780203582

--- duke.js
+++ duke.js
@@ -81,13 +81,13 @@ var chatJson = {
             "user": "duke"
         },
         {
-            "time": "22 minutes ago",
+            "time": "23 minutes ago",
             "text": "If you send me the link to the possible website that you mentioned then I might be able to take a look at it.(more in the sheets)",
             "user": "duke"
         },
         {
-            "time": "A few seconds ago",
-            "text": "And I have replied :) Also sorry for the spam everything 😭 I feel really weird talking to no one yet talking.",
+            "time": "a minute ago",
+            "text": "And I have replied :) Also sorry for the spam everything :sob: I feel really weird talking to no one yet talking.",
             "user": "duke"
         }
     ],
